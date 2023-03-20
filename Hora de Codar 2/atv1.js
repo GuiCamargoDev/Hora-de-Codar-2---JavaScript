49b0b17cc907b43f16b1d340e73ce8b90e0431ba
GuiCamargoDev <124837745+GuiCamargoDev@users.noreply.github.com>
2023-03-20 12:15:22 -0300

    Add files via upload

---
+++ Hora de Codar 2/atv1.js
@@ -0,0 +1,8 @@
+var primeiro_numero = prompt("Digite um número");
+var segundo_numero = prompt("Digite outro número");
+
+if (primeiro_numero > segundo_numero) { document.write("O primeiro número é o maior") }
+
+else if (primeiro_numero < segundo_numero) { document.write("O segundo número é o maior") }
+
+else if (primeiro_numero == segundo_numero) { alert("ERRO") }
